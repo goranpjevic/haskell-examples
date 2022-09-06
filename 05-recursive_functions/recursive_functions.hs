cf292f7cdfbebc29a67acd773c6271b007b3c6fe
Goran Pjević <67331732+goranpjevic@users.noreply.github.com>
2022-09-06 07:43:17 +0200

    add the fifth example file

---
+++ 05-recursive_functions/recursive_functions.hs
@@ -0,0 +1,40 @@
+-- are all logical values in a list true
+and' :: [Bool] -> Bool
+and' [] = True
+and' (x:xs) | x == False = False
+            | otherwise  = and' xs
+
+-- concatenate a list of lists
+concat' :: [[a]] -> [a]
+concat' [] = []
+concat' (xs:xss) = xs ++ concat' xss
+
+-- make a list of n identical elements
+replicate' :: Int -> a -> [a]
+replicate' 0 x = []
+replicate' n x = x : replicate' (n-1) x
+
+-- get the nth element of a list
+(!!!) :: [a] -> Int -> a
+(x:xs) !!! 0 = x
+(x:xs) !!! n = xs !!! (n-1)
+
+-- is a value an element of a list
+elem' :: Eq a => a -> [a] -> Bool
+elem' _ [] = False
+elem' n (x:xs) | x == n = True
+               | otherwise = elem' n xs
+
+-- merge two sorted lists into one sorted list
+merge :: Ord a => [a] -> [a] -> [a]
+merge xs [] = xs
+merge [] ys = ys
+merge (x:xs) (y:ys) | x <= y = x : merge xs (y:ys)
+                    | otherwise = y : merge (x:xs) ys
+
+-- merge sort
+msort :: Ord a => [a] -> [a]
+msort [] = []
+msort [x] = [x]
+msort xs = merge (msort (take l xs)) (msort (drop l xs))
+           where l = div (length xs) 2
